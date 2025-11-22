79b40e600888a3d9b95fc559388670e7f90c789d
AmeneRenz <renzamene0825@gmail.com>
2025-11-23 04:43:26 +0800

    Updated with Light Flickering

---
+++ Assets/TutorialInfo/Scripts/LightFlicker.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour
+{
+    private Light light;
+
+    public float minIntensity = .5f;
+    public float maxIntensity = 5.0f;
+    public float flickerSpeed = 0.1f;
+
+    private void Start()
+    {
+        light = GetComponent<Light>();
+        InvokeRepeating("Flicker", 0f, flickerSpeed);
+    }
+
+    private void Flicker()
+    {
+        float randomIntensity = Random.Range(minIntensity, maxIntensity);
+        light.intensity = randomIntensity;
+    }
+}
